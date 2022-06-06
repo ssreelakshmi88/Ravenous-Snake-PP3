55e51fd7ecbe5af88c462b00a9629a948b7643d5
Sreelakshmi Sasidharan <sreelakshmi.s2@gmail.com>
2022-06-06 20:09:03 +0000

    Add import cursus module

--- run.py
+++ run.py
@@ -1,3 +1,16 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+"""
+The curses module is used to handle user keypresses,
+allowing them to move the snake
+"""
+
+import curses
+
+# setting window
+sc = curses.initscr()
+win = curses.newwin(20, 80, 0, 0)
+win.keypad(1)
+curses.noecho()
+curses.curs_set(0)
+
+
+# logic of playing game
