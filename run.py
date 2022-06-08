9fce12cead02b090574ebb3d26239a4efee85422
Sreelakshmi Sasidharan <sreelakshmi.s2@gmail.com>
2022-06-08 09:15:36 +0000

    Add snake and food position

--- run.py
+++ run.py
@@ -8,9 +8,16 @@ import curses
 # setting window
 sc = curses.initscr()
 win = curses.newwin(20, 80, 0, 0)
-win.keypad(1)
-curses.noecho()
-curses.curs_set(0)
+win.keypad(1)  # to recognize the key presses
+curses.noecho()  # prevents user key presses registering on terminal window
+
+# Snake and food position
+
+food_position = [8, 55]
+snake_head = [10, 15]
+snake_position = [[15, 10], [14, 10], [13, 10]]
+
+SCORE = 0
 
 
-# logic of playing game
+win.addch(food_position[0], food_position[1], "█")
